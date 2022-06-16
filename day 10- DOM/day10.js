8f157cea81883c020b33f86ee2f73e1cef1067a9
unknown <you@example.com>
2022-06-16 12:33:29 +0545

    tasks

---
+++ day 10- DOM/day10.js
@@ -0,0 +1,6 @@
+function(){
+    const question = document.getElementById('question');
+    const answer = document.getElementById('answer');
+    const timer = document.getElementById('timer')
+}
+const questionbank
